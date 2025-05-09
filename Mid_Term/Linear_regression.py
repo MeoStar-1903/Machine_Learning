d7be041b28e795bef3c4d3d5d6b37f5f28aed387
Nguyễn Đức Minh <meomunblabla@gmail.com>
2025-05-09 21:16:42 +0700

    Add files via upload

--- Mid_Term/Linear_regression.py
+++ Mid_Term/Linear_regression.py
@@ -25,11 +25,27 @@ ss_total = np.sum((y - np.mean(y)) ** 2)
 ss_res = np.sum((y - y_pred) ** 2)
 r2 = 1 - (ss_res / ss_total)
 
-#Print đánh giá mô hình
+##Print đánh giá mô hình
 #print("Hệ số beta (bao gồm intercept):")
 #print(beta.flatten())
 #print("Mean squared Error:", mse)
 #print ("R^2 score:", r2)
+#import matplotlib.pyplot as plt
+
+## Flatten y and y_pred for plotting
+#y_true = y.flatten()
+#y_pred_flat = y_pred.flatten()
+
+## Plot
+#plt.scatter(y_true, y_pred_flat, color="blue", alpha=0.6, label="Predictions")
+#lt.plot([min(y_true), max(y_true)], [min(y_true), max(y_true)], color="red", linestyle="--", label="Perfect prediction")
+
+#plt.xlabel("Actual Price (billion VND)")
+#plt.ylabel("Predicted Price (billion VND)")
+#plt.title("Linear Regression: Actual vs Predicted Prices")
+#plt.legend()
+#plt.grid(True)
+#plt.show()
 
 #Dự đoán giá nhà cho một số mẫu mới
 new_house = np.array([[100, 1, 3, 1], [150, 0, 4, 1], [80, 1, 2, 0]])
@@ -40,3 +56,7 @@ new_house_with_bias = np.hstack([np.ones((new_house.shape[0], 1)), new_house])
 predict = new_house_with_bias.dot(beta)
 print("Dự đoán giá nhà cho các mẫu mới(tỷ): ")
 print(predict)
+
+
+
+

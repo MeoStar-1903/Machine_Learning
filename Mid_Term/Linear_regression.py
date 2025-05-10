5976263147397c748c5efcd9f6bec2a8c7072a60
Nguyễn Đức Minh <meomunblabla@gmail.com>
2025-05-10 20:25:03 +0700

    Add files via upload

--- Mid_Term/Linear_regression.py
+++ Mid_Term/Linear_regression.py
@@ -2,44 +2,52 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Đọc dữ liệu từ file csv
+#Read data from file csv
 df = pd.read_csv("Data/gia_nha.csv")
 
-#Chuẩn bị dữ liệu
+#Preprocess data
 X = df[["Dien_tich_m2", "Co_giay_to", "So_phong", "Co_noi_that"]].values
 y = df["Gia_nha_ty"].values.reshape(-1, 1)
 
-X_bias = np.hstack([np.ones((X.shape[0], 1)), X])  # Thêm cột bias vào X
+# Add bias term (intercept) to the input features
+# Bias term is a column of ones added to the input features
+X_bias = np.hstack([np.ones((X.shape[0], 1)), X])  
 
-#Tính hệ số beta the công thức đóng
+#Calculate beta using Normal Equation
+# beta = (X^T * X)^(-1) * X^T * y
 XtX = X_bias.T.dot(X_bias)
 XtX_inv = np.linalg.inv(XtX)
 Xty = X_bias.T.dot(y)
 beta = XtX_inv.dot(Xty)
 
-#Dự đoán giá nhà
+#Prediction
 y_pred = X_bias.dot(beta)
 
-#Đánh giá mô hình
-mse = np.mean((y - y_pred) ** 2)
-ss_total = np.sum((y - np.mean(y)) ** 2)
-ss_res = np.sum((y - y_pred) ** 2)
-r2 = 1 - (ss_res / ss_total)
-
-#Dự đoán giá nhà cho một số mẫu mới
+#Predict new data
+#Create new data for prediction
 new_house = np.array([[100, 1, 3, 1], [150, 0, 4, 1], [80, 1, 2, 0]])
 
-# Thêm cột bias vào new_data
+#Add bias term to new data
 new_house_with_bias = np.hstack([np.ones((new_house.shape[0], 1)), new_house]) 
 
+#Predict price for new data
+#Use the same beta calculated from the training data
 predict = new_house_with_bias.dot(beta)
 print("Dự đoán giá nhà cho các mẫu mới(tỷ): ")
 print(predict)
 
 
 
-"""#Print đánh giá mô hình
-print("Hệ số beta (bao gồm intercept):")
+"""
+#Rate the model
+#Calculate Mean Squared Error (MSE) and R^2 score
+mse = np.mean((y - y_pred) ** 2)
+ss_total = np.sum((y - np.mean(y)) ** 2)
+ss_res = np.sum((y - y_pred) ** 2)
+r2 = 1 - (ss_res / ss_total)
+
+#Print model parameters
+print("Hệ số beta (not include intercept):")
 print(beta.flatten())
 print("Mean squared Error:", mse)
 print ("R^2 score:", r2)
